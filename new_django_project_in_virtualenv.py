6f03f142c6a4c15793731d3cc73fd0a1ca31bd30
Harry <hjwp2@cantab.net>
2017-01-11 12:26:31 +0000

    T5300 first real tests, check argument defaults. by: Harry

--- new_django_project_in_virtualenv.py
+++ new_django_project_in_virtualenv.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3.5
-# Copyright (c) 2016 PythonAnywhere LLP.
-# All Rights Reserved
-#
-
 """Create a new Django webapp with a virtualenv.  Defaults to
 your free domain, the latest version of Django and Python 3.5
 
@@ -19,7 +15,7 @@ from docopt import docopt
 import getpass
 
 
-def create_virtualenv(name, python_version):
+def create_virtualenv(name, python_version, django_version):
     pass
 
 
@@ -32,16 +28,11 @@ def create_webapp(domain, project_path):
 
 
 def main(domain, django_version, python_version):
-    return
     if domain == 'your-username.pythonanywhere.com':
         username = getpass.getuser()
         domain = '{}.pythonanywhere.com'.format(username)
-    if django_version == 'latest':
-        django_version = None
+    create_virtualenv(domain, python_version, django_version)
 
-    virtualenv_path = create_virtualenv()
-    project_path = start_django_project()
-    create_webapp()
 
 
 if __name__ == '__main__':
--- tests/test_new_django_project_in_virtualenv.py
+++ tests/test_new_django_project_in_virtualenv.py
@@ -1,4 +1,5 @@
-import pytest
+import getpass
+from unittest.mock import patch, call
 
 from new_django_project_in_virtualenv import (
     create_virtualenv,
@@ -8,6 +9,28 @@ from new_django_project_in_virtualenv import (
 )
 
 
-def test_smoke():
-    assert False
+@patch('new_django_project_in_virtualenv.create_virtualenv')
+@patch('new_django_project_in_virtualenv.start_django_project')
+@patch('new_django_project_in_virtualenv.create_webapp')
+class TestMain:
+
+    def test_calls_create_virtualenv(
+        self, mock_create_webapp, mock_start_django_project,
+        mock_create_virtualenv
+    ):
+        main('domain', 'django.version', 'python.version')
+        assert mock_create_virtualenv.call_args == call(
+            'domain', 'python.version', 'django.version'
+        )
+
+
+    def test_domain_defaults_to_using_current_username(
+        self, mock_create_webapp, mock_start_django_project,
+        mock_create_virtualenv
+    ):
+        username = getpass.getuser()
+        main('your-username.pythonanywhere.com', 'django.version', 'python.version')
+        assert mock_create_virtualenv.call_args == call(
+            username + '.pythonanywhere.com', 'python.version', 'django.version'
+        )
 
