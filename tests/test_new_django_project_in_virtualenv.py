f9672993817e88e03b71ea957834f8f90f3311b7
Harry <hjwp2@cantab.net>
2017-01-18 21:43:26 +0000

    move all slow tests into one semifunctional thing

--- tests/test_new_django_project_in_virtualenv.py
+++ tests/test_new_django_project_in_virtualenv.py
@@ -72,6 +72,38 @@ class TestMain:
         )
 
 
+class TestMainSemiFunctional:
+
+    @pytest.mark.slowtest
+    def test_creates_django_project_in_virtualenv_with_hacked_settings_and_static_files(
+        self, fake_home, virtualenvs_folder, api_responses, api_token
+    ):
+
+        webapps_url = API_ENDPOINT.format(username=getpass.getuser())
+        webapp_url = API_ENDPOINT.format(username=getpass.getuser()) + 'mydomain.com/'
+        reload_url = webapp_url + 'reload'
+        api_responses.add(responses.POST, webapps_url, status=201, body=json.dumps({'status': 'OK'}))
+        api_responses.add(responses.PATCH, webapp_url, status=200)
+        api_responses.add(responses.POST, reload_url, status=200, body=json.dumps({'status': 'OK'}))
+
+        with patch('new_django_project_in_virtualenv.update_wsgi_file'):
+            main('mydomain.com', '1.9.2', '2.7')
+
+        django_version = subprocess.check_output([
+            os.path.join(virtualenvs_folder, 'mydomain.com/bin/python'),
+            '-c'
+            'import django; print(django.get_version())'
+        ]).decode().strip()
+        assert django_version == '1.9.2'
+
+        with open(os.path.join(fake_home, 'mydomain.com/mysite/settings.py')) as f:
+            lines = f.read().split('\n')
+        assert "MEDIA_ROOT = os.path.join(BASE_DIR, 'media')" in lines
+        assert "ALLOWED_HOSTS = ['mydomain.com']" in lines
+
+        assert 'base.css' in os.listdir(os.path.join(fake_home, 'mydomain.com/static/admin/css'))
+
+
 
 class TestCreateVirtualenv:
 
@@ -103,49 +135,10 @@ class TestCreateVirtualenv:
         assert response == os.path.join(virtualenvs_folder, 'domain.com')
 
 
-    @pytest.mark.slowtest
-    def test_actually_creates_a_virtualenv_with_right_django_version_in(self, virtualenvs_folder):
-        domain = 'mydomain.com'
-        create_virtualenv(domain, '2.7', '1.9')
-
-        assert domain in os.listdir(virtualenvs_folder)
-        django_version = subprocess.check_output([
-            os.path.join(virtualenvs_folder, domain, 'bin/python'),
-            '-c'
-            'import django; print(django.get_version())'
-        ]).decode().strip()
-        assert django_version == '1.9'
 
 
 
 class TestStartDjangoProject:
-
-    @pytest.mark.slowtest
-    def test_actually_creates_a_django_project(self, test_virtualenv, fake_home):
-        start_django_project('mydomain.com', test_virtualenv)
-        assert 'mydomain.com' in os.listdir(fake_home)
-        assert 'settings.py' in os.listdir(os.path.join(fake_home, 'mydomain.com/mysite'))
-
-
-    @pytest.mark.slowtest
-    def test_adds_STATIC_and_MEDIA_config_to_settings(self, test_virtualenv, fake_home):
-        start_django_project('mydomain.com', test_virtualenv)
-        with open(os.path.join(fake_home, 'mydomain.com/mysite/settings.py')) as f:
-            contents = f.read()
-
-        print(contents)
-        lines = contents.split('\n')
-        assert "STATIC_URL = '/static/'" in lines
-        assert "MEDIA_URL = '/media/'" in lines
-        assert "STATIC_ROOT = os.path.join(BASE_DIR, 'static')" in lines
-        assert "MEDIA_ROOT = os.path.join(BASE_DIR, 'media')" in lines
-        assert "ALLOWED_HOSTS = ['mydomain.com']" in lines
-
-
-    @pytest.mark.slowtest
-    def test_has_run_collectstatic(self, test_virtualenv, fake_home):
-        start_django_project('mydomain.com', test_virtualenv)
-        assert 'base.css' in os.listdir(os.path.join(fake_home, 'mydomain.com/static/admin/css'))
 
 
     def test_calls_update_settings_file(self, mock_subprocess, fake_home):
