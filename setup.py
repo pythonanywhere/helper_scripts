d1c0158587d8a8d511481aee6e0f5a5d5e377aca
Giles Thomas <giles@pythonanywhere.com>
2018-11-27 17:44:57 +0000

    Specify long description format so that the PyPI page for the module looks better.  by: Conrad, Giles

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version='0.7',
     description='PythonAnywhere helper tools for users',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/pythonanywhere/helper_scripts/',
     author='PythonAnywhere LLP',
     author_email='developers@pythonanywhere.com',
