a36300e9ae4805f3ba2c7ac02fac1e7814f54317
Filip Lajszczak <filip.lajszczak@gmail.com>
2020-12-04 13:50:00 +0000

    #5 new cli delete web app logs.

--- tests/test_cli_webapp.py
+++ tests/test_cli_webapp.py
@@ -1,11 +1,23 @@
 import getpass
-from unittest.mock import Mock, call
+from unittest.mock import call
 
+import pytest
 from typer.testing import CliRunner
 
 from cli.webapp import app
 
 runner = CliRunner()
+
+
+@pytest.fixture
+def mock_webapp(mocker):
+    mock_webapp = mocker.patch("cli.webapp.Webapp")
+    mock_webapp.return_value.get_log_info.return_value = {
+        "access": [0, 1, 2],
+        "error": [0, 1, 2],
+        "server": [0, 1, 2],
+    }
+    return mock_webapp
 
 
 def test_create_calls_all_stuff_in_right_order(mocker):
@@ -38,9 +50,7 @@ def test_create_calls_all_stuff_in_right_order(mocker):
     )
 
 
-def test_reload(mocker):
-    mock_webapp = mocker.patch("cli.webapp.Webapp")
-    mocker.patch("cli.webapp.ensure_domain", Mock(side_effect=lambda x: x))
+def test_reload(mock_webapp):
     domain_name = "foo.bar.baz"
 
     result = runner.invoke(app, ["reload", "-d", domain_name])
@@ -48,3 +58,79 @@ def test_reload(mocker):
     assert f"{domain_name} has been reloaded" in result.stdout
     mock_webapp.assert_called_once_with(domain_name)
     assert mock_webapp.return_value.method_calls == [call.reload()]
+
+
+def test_delete_all_logs(mock_webapp):
+    domain_name = "foo.bar.baz"
+
+    result = runner.invoke(
+        app,
+        [
+            "delete-logs",
+            "-d",
+            "foo.bar.baz",
+        ],
+    )
+
+    mock_webapp.assert_called_once_with(domain_name)
+    assert mock_webapp.return_value.delete_log.call_args_list == [
+        call("access", 0),
+        call("access", 1),
+        call("access", 2),
+        call("error", 0),
+        call("error", 1),
+        call("error", 2),
+        call("server", 0),
+        call("server", 1),
+        call("server", 2),
+    ]
+    assert "All done!" in result.stdout
+
+
+def test_delete_all_server_logs(mock_webapp):
+    domain_name = "foo.bar.baz"
+
+    result = runner.invoke(
+        app,
+        [
+            "delete-logs",
+            "-d",
+            "foo.bar.baz",
+            "-t",
+            "server",
+        ],
+    )
+
+    mock_webapp.assert_called_once_with(domain_name)
+    assert mock_webapp.return_value.delete_log.call_args_list == [
+        call("server", 0),
+        call("server", 1),
+        call("server", 2),
+    ]
+    assert "All done!" in result.stdout
+
+
+def test_delete_one_server_logs(mock_webapp):
+    domain_name = "foo.bar.baz"
+
+    result = runner.invoke(
+        app, ["delete-logs", "-d", "foo.bar.baz", "-t", "server", "-i", "2"]
+    )
+
+    mock_webapp.assert_called_once_with(domain_name)
+    mock_webapp.return_value.delete_log.assert_called_once_with("server", 2)
+    assert "All done!" in result.stdout
+
+
+def test_delete_all_current_logs(mock_webapp):
+    domain_name = "foo.bar.baz"
+
+    result = runner.invoke(app, ["delete-logs", "-d", "foo.bar.baz", "-i", "0"])
+
+    mock_webapp.assert_called_once_with(domain_name)
+    assert mock_webapp.return_value.delete_log.call_args_list == [
+        call("access", 0),
+        call("error", 0),
+        call("server", 0),
+    ]
+    assert "All done!" in result.stdout
