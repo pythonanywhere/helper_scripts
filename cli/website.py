67408b1e060e2c07ce3867ae2e3b3f5f5bc90779
Giles Thomas <giles@pythonanywhere.com>
2024-07-03 16:42:21 +0100

    PA-655 stub subcommands for get, reload, and delete.  by: Piotr, Giles

--- cli/website.py
+++ cli/website.py
@@ -29,5 +29,43 @@ def create(
 
 
 @app.command()
-def delete():
+def get(
+    domain_name: str = typer.Option(
+        None,
+        "-d",
+        "--domain",
+        help="Get details for domain name, eg. yourusername.pythonanywhere.com or www.mydomain.com",
+    )
+):
+    """If no domain name is specified, list all domains.  Otherwise get details for specified domain"""
+    pass
+
+
+@app.command()
+def reload(
+    domain_name: Annotated[
+        str,
+        typer.Option(
+            "-d",
+            "--domain",
+            help="Domain name, eg. yourusername.pythonanywhere.com or www.mydomain.com",
+        )
+    ],
+):
+    """Reload the website at the given domain"""
+    pass
+
+
+@app.command()
+def delete(
+    domain_name: Annotated[
+        str,
+        typer.Option(
+            "-d",
+            "--domain",
+            help="Domain name, eg. yourusername.pythonanywhere.com or www.mydomain.com",
+        )
+    ],
+):
+    """Delete the website at the given domain"""
     pass
--- tests/test_cli_website.py
+++ tests/test_cli_website.py
@@ -40,7 +40,7 @@ def test_create_without_command_barfs():
     assert "Missing option" in result.stdout
 
 
-def test_create_with_domain_and_command_does_something():
+def test_create_with_domain_and_command_creates_it():
     result = runner.invoke(
         app,
         [
@@ -52,5 +52,78 @@ def test_create_with_domain_and_command_does_something():
         ],
     )
     assert result.exit_code == 0
+    assert False, "TODO"
 
+
+def test_get_with_no_domain_lists_websites():
+    result = runner.invoke(
+        app,
+        [
+            "get",
+        ],
+    )
+    assert result.exit_code == 0
+    assert False, "TODO"
+
+
+def test_get_with_domain_gives_details_for_domain():
+    result = runner.invoke(
+        app,
+        [
+            "get",
+            "-d",
+            "www.domain.com",
+        ],
+    )
+    print(result.stdout)
+    assert result.exit_code == 0
+    assert False, "TODO"
+
+
+def test_reload_with_no_domain_barfs():
+    result = runner.invoke(
+        app,
+        [
+            "reload",
+        ],
+    )
+    assert result.exit_code != 0
+    assert "Missing option" in result.stdout
+
+
+def test_reload_with_domain_reloads():
+    result = runner.invoke(
+        app,
+        [
+            "reload",
+            "-d",
+            "www.domain.com",
+        ],
+    )
+    assert result.exit_code == 0
+    assert False, "TODO"
+
+
+def test_delete_with_no_domain_barfs():
+    result = runner.invoke(
+        app,
+        [
+            "delete",
+        ],
+    )
+    assert result.exit_code != 0
+    assert "Missing option" in result.stdout
+
+
+def test_delete_with_domain_deletes_it():
+    result = runner.invoke(
+        app,
+        [
+            "delete",
+            "-d",
+            "www.domain.com",
+        ],
+    )
+    assert result.exit_code == 0
+    assert False, "TODO"
 
